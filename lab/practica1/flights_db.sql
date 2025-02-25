dd90a01313c6dfd8b3c0bf1fe9fff22b85648a86
Juan Gomez <juan.e.g.f@outlook.com>
2025-02-24 21:01:12 -0600

    bugfix: db script

--- lab/practica1/flights_db.sql
+++ lab/practica1/flights_db.sql
@@ -1,143 +1,211 @@
-CREATE TABLE [fact_flight] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[sk_airport_continent] INTEGER NOT NULL,
-	[sk_departure_country] INTEGER NOT NULL,
-	[sk_departure_airport] INTEGER NOT NULL,
-	[sk_departure_date] INTEGER NOT NULL,
-	[sk_arrival_airport] INTEGER NOT NULL,
-	[sk_pilot] INTEGER NOT NULL,
-	[sk_passenger] INTEGER NOT NULL,
-	[sk_flight_status] INTEGER NOT NULL,
-	PRIMARY KEY([id])
+USE master
+GO
+
+DROP DATABASE IF EXISTS seminario
+GO
+
+CREATE DATABASE seminario
+GO
+
+USE seminario
+GO
+
+DROP SCHEMA IF EXISTS practica1
+GO
+
+CREATE SCHEMA practica1
+GO
+
+IF OBJECT_ID('practica1.dim_flight_status', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_flight_status;
+GO
+
+IF OBJECT_ID('practica1.dim_nationality', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_nationality;
+GO
+
+IF OBJECT_ID('practica1.dim_age', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_age;
+GO
+
+IF OBJECT_ID('practica1.dim_gender', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_gender;
+GO
+
+IF OBJECT_ID('practica1.dim_passenger', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_passenger;
+GO
+
+IF OBJECT_ID('practica1.dim_pilot', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_pilot;
+GO
+
+IF OBJECT_ID('practica1.dim_arrival_airport', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_arrival_airport;
+GO
+
+IF OBJECT_ID('practica1.dim_departure_date', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_departure_date;
+GO
+
+IF OBJECT_ID('practica1.dim_airport_continent', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_airport_continent;
+GO
+
+IF OBJECT_ID('practica1.dim_departure_country', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_departure_country;
+GO
+
+IF OBJECT_ID('practica1.dim_departure_airport', 'U') IS NOT NULL
+    DROP TABLE practica1.dim_departure_airport;
+GO
+
+IF OBJECT_ID('practica1.fact_flight', 'U') IS NOT NULL
+    DROP TABLE practica1.fact_flight;
+GO
+
+
+CREATE TABLE practica1.dim_departure_airport (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	airport_name NVARCHAR(32) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_departure_airport] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[airport_name] NVARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_departure_country (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	country_name NVARCHAR(16) NOT NULL,
+	country_code NVARCHAR(4) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_departure_country] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[country_name] NVARCHAR NOT NULL,
-	[country_code] NVARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_airport_continent (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	continent_code NVARCHAR(4) NOT NULL,
+	continent_name NVARCHAR(8) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_airport_continent] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[continent_code] VARCHAR NOT NULL,
-	[continent_name] VARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_departure_date (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	departure_date DATE NOT NULL,
+	month INTEGER NOT NULL,
+	day INTEGER NOT NULL,
+	year INTEGER NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_departure_date] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[departure_date] DATE NOT NULL,
-	[month] INTEGER NOT NULL,
-	[day] INTEGER NOT NULL,
-	[year] INTEGER NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_arrival_airport (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	arrival_airport NVARCHAR(4) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_arrival_airport] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[arrival_airport] NVARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_pilot (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	pilot_name NVARCHAR(32) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_pilot] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[pilot_name] NVARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_passenger (
+	sk_id NVARCHAR NOT NULL UNIQUE,
+	sk_gender INTEGER NOT NULL,
+	sk_age INTEGER NOT NULL,
+	sk_nationality INTEGER NOT NULL,
+	first_name NVARCHAR(32) NOT NULL,
+	last_name NVARCHAR(32) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_passenger] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[sk_gender] INTEGER NOT NULL,
-	[sk_age] INTEGER NOT NULL,
-	[sk_nationality] INTEGER NOT NULL,
-	[name] VARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_gender (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	gender NVARCHAR(4) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_gender] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[gender] VARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_age (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	age INTEGER NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_age] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[age] INTEGER NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_nationality (
+	sk_id INTEGER NOT NULL IDENTITY UNIQUE,
+	nationality NVARCHAR(32) NOT NULL,
+	PRIMARY KEY(sk_id)
 );
 GO
 
-CREATE TABLE [dim_nationality] (
-	[sk_id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[nationality] NVARCHAR NOT NULL,
-	PRIMARY KEY([sk_id])
+CREATE TABLE practica1.dim_flight_status (
+	id INTEGER NOT NULL IDENTITY UNIQUE,
+	flight_status NVARCHAR(8) NOT NULL,
+	PRIMARY KEY(id)
 );
 GO
 
-CREATE TABLE [dim_flight_status] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
-	[flight_status] VARCHAR NOT NULL,
-	PRIMARY KEY([id])
+CREATE TABLE practica1.fact_flight (
+	id INTEGER NOT NULL IDENTITY UNIQUE,
+	sk_airport_continent INTEGER NOT NULL,
+	sk_departure_country INTEGER NOT NULL,
+	sk_departure_airport INTEGER NOT NULL,
+	sk_departure_date INTEGER NOT NULL,
+	sk_arrival_airport INTEGER NOT NULL,
+	sk_pilot INTEGER NOT NULL,
+	sk_passenger NVARCHAR NOT NULL,
+	sk_flight_status INTEGER NOT NULL,
+	PRIMARY KEY(id)
 );
 GO
 
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_airport_continent]) REFERENCES [dim_airport_continent]([sk_id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_airport_continent) REFERENCES practica1.dim_airport_continent(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_departure_country]) REFERENCES [dim_departure_country]([sk_id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_departure_country) REFERENCES practica1.dim_departure_country(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_departure_airport]) REFERENCES [dim_departure_airport]([sk_id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_departure_airport) REFERENCES practica1.dim_departure_airport(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_departure_date]) REFERENCES [dim_departure_date]([sk_id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_departure_date) REFERENCES practica1.dim_departure_date(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_arrival_airport]) REFERENCES [dim_arrival_airport]([sk_id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_arrival_airport) REFERENCES practica1.dim_arrival_airport(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_pilot]) REFERENCES [dim_pilot]([sk_id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_pilot) REFERENCES practica1.dim_pilot(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_passenger]) REFERENCES [dim_passenger]([sk_id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_passenger) REFERENCES practica1.dim_passenger(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [dim_passenger]
-ADD FOREIGN KEY([sk_gender]) REFERENCES [dim_gender]([sk_id])
+ALTER TABLE practica1.dim_passenger
+ADD FOREIGN KEY(sk_gender) REFERENCES practica1.dim_gender(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [dim_passenger]
-ADD FOREIGN KEY([sk_age]) REFERENCES [dim_age]([sk_id])
+ALTER TABLE practica1.dim_passenger
+ADD FOREIGN KEY(sk_age) REFERENCES practica1.dim_age(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [dim_passenger]
-ADD FOREIGN KEY([sk_nationality]) REFERENCES [dim_nationality]([sk_id])
+ALTER TABLE practica1.dim_passenger
+ADD FOREIGN KEY(sk_nationality) REFERENCES practica1.dim_nationality(sk_id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_flight]
-ADD FOREIGN KEY([sk_flight_status]) REFERENCES [dim_flight_status]([id])
+ALTER TABLE practica1.fact_flight
+ADD FOREIGN KEY(sk_flight_status) REFERENCES practica1.dim_flight_status(id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
