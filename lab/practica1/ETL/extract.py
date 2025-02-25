f1d9391901d7ffcd8ef67f5e656a8e7e7c58bbcb
Juan Gomez <juan.e.g.f@outlook.com>
2025-02-25 16:12:32 -0600

    chore: format messages

--- lab/practica1/ETL/extract.py
+++ lab/practica1/ETL/extract.py
@@ -4,23 +4,16 @@ def extract():
 
     try:
         dataframe = pd.read_csv("C:/Users/Juan Enrique/seminario2/lab/practica1/VuelosDataSet.csv")
-        print("Cantidad de registros:", len(dataframe))
-
-        input("Para continuar presione \"Enter\"...")
+        print("\NCantidad de registros:", len(dataframe))
 
         print("\nPrimeras 5 filas:")
         print(dataframe.head())
 
-        input("Para continuar presione \"Enter\"...")
-
         print("\nUltimas 5 filas:")
-        print(dataframe.tail())
-
-        input("Para continuar presione \"Enter\"...")
+        print(dataframe.tail(), '\n')
 
         return dataframe
 
     except Exception as e:
         print(f"Error leyendo el archivo: {e}")
-        input("Para continuar presione \"Enter\"...")
-        return 0
+        return 
