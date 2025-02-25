b03df0332a1a1cc56ab3bbaa110877dedc06b439
Juan Gomez <juan.e.g.f@outlook.com>
2025-02-25 16:13:36 -0600

    chore: add check and format messages

--- lab/practica1/ETL/load.py
+++ lab/practica1/ETL/load.py
@@ -1,5 +1,6 @@
 import database
 from tqdm import tqdm
+import database
 
 def load(data):
     dim_airport_continent = data[0]
@@ -16,6 +17,14 @@ def load(data):
     fact_flight = data[11]
 
     try:
+        db_result = database.cursor.execute("SELECT 1 FROM sys.databases WHERE name = 'seminario'").fetchall()
+
+        if len(db_result) == 0:
+            print('\nno existe ningun modelo, crea el modelo antes de cargar los datos\n')
+            return
+
+        database.cursor.execute("USE seminario")
+
         for _, row in tqdm(dim_airport_continent.iterrows(), total = len(dim_airport_continent), desc = "Insertando datos de continentes"):
             database.cursor.execute("""
                 IF NOT EXISTS (SELECT 1 FROM practica1.dim_airport_continent WHERE sk_id = ?)
@@ -27,6 +36,9 @@ def load(data):
                 """,
                 row['sk_id'], row['sk_id'], row['continent_code'], row['continent_name']
             )
+
+
+        print("Continentes insertados\n")
 
         for _, row in tqdm(dim_departure_country.iterrows(), total = len(dim_departure_country), desc = "Insertando datos de paises de salidas"):
             database.cursor.execute("""
@@ -40,17 +52,21 @@ def load(data):
                 row['sk_id'], row['sk_id'], row['country_name'], row['country_code']
             )
 
+        print("Paises de salida insertados\n")
+
         for _, row in tqdm(dim_departure_airport.iterrows(), total = len(dim_departure_airport), desc = "Insertando datos de aeropuertos de salidas"):
             database.cursor.execute("""
                 IF NOT EXISTS (SELECT 1 FROM practica1.dim_departure_airport WHERE sk_id = ?)
                 BEGIN
                     INSERT INTO  practica1.dim_departure_airport (sk_id, airport_name)
-                    VALUES (?, ?, ?)
+                    VALUES (?, ?)
                 END
 
                 """,
                 row['sk_id'], row['sk_id'], row['airport_name']
             )
+
+        print("Aeropuertos de salida insertados\n")
 
         for _, row in tqdm(dim_departure_date.iterrows(), total = len(dim_departure_date), desc = "Insertando datos de fechas de salidas"):
             database.cursor.execute("""
@@ -65,6 +81,8 @@ def load(data):
                 row['sk_id'], row['sk_id'], row['departure_date'], row['month'], row['day'], row['year']
             )
 
+        print("Fechas de salida insertados\n")
+
         for _, row in tqdm(dim_arrival_airport.iterrows(), total = len(dim_arrival_airport), desc = "Insertando datos de aeropuertos de destino"):
             database.cursor.execute("""
                 IF NOT EXISTS (SELECT 1 FROM practica1.dim_arrival_airport WHERE sk_id = ?)
@@ -76,6 +94,8 @@ def load(data):
                 """,
                 row['sk_id'], row['sk_id'], row['arrival_airport']
             )
+
+        print("Aeropuertos de destino insertados\n")
 
         for _, row in tqdm(dim_pilot.iterrows(), total = len(dim_pilot), desc = "Insertando datos de pilotos"):
             database.cursor.execute("""
@@ -89,6 +109,8 @@ def load(data):
                 row['sk_id'], row['sk_id'], row['pilot_name']
             )
 
+        print("Pilotos insertados\n")
+
         for _, row in tqdm(dim_gender.iterrows(), total = len(dim_gender), desc = "Insertando datos de genero"):
             database.cursor.execute("""
                 IF NOT EXISTS (SELECT 1 FROM practica1.dim_gender WHERE sk_id = ?)
@@ -100,6 +122,8 @@ def load(data):
                 """,
                 row['sk_id'], row['sk_id'], row['gender']
             )
+
+        print("Generos insertados\n")
 
         for _, row in tqdm(dim_age.iterrows(), total = len(dim_age), desc = "Insertando datos de edad"):
             database.cursor.execute("""
@@ -113,6 +137,8 @@ def load(data):
                 row['sk_id'], row['sk_id'], row['age']
             )
 
+        print("Edades insertados\n")
+
         for _, row in tqdm(dim_nationality.iterrows(), total = len(dim_nationality), desc = "Insertando datos de nacionalidades"):
             database.cursor.execute("""
                 IF NOT EXISTS (SELECT 1 FROM practica1.dim_nationality WHERE sk_id = ?)
@@ -124,6 +150,8 @@ def load(data):
                 """,
                 row['sk_id'], row['sk_id'], row['nationality']
             )
+
+        print("Nacionalidades insertadas\n")
 
         for _, row in tqdm(dim_passenger.iterrows(), total = len(dim_passenger), desc = "Insertando datos de pasajeros"):
             database.cursor.execute("""
@@ -137,6 +165,8 @@ def load(data):
                 row['sk_id'], row['sk_id'], row['sk_gender'], row['sk_age'], row['sk_nationality'], row['first_name'], row['last_name']
             )
 
+        print("Pasajeros insertados\n")
+
         for _, row in tqdm(dim_flight_status.iterrows(), total = len(dim_flight_status), desc = "Insertando datos de estados de vuelo"):
             database.cursor.execute("""
                 IF NOT EXISTS (SELECT 1 FROM practica1.dim_flight_status WHERE sk_id = ?)
@@ -149,6 +179,8 @@ def load(data):
                 row['sk_id'], row['sk_id'], row['flight_status']
             )
 
+        print("estados de vuelos insertados\n")
+        print("Exito, todos los datos se han cargado\n")
+
     except Exception as e:
-        print(row)
         print(f"Error al insertar datos: {e}")
