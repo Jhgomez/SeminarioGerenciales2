d246b3475711d8363905825b0c6c2ec951dc241e
Juan Gomez <juan.e.g.f@outlook.com>
2025-03-20 20:56:52 -0600

    chore: add script

--- lab/project1/SQLServer/DataWarehouse/DataDefinitionLanguage/dw_ddl.sql
+++ lab/project1/SQLServer/DataWarehouse/DataDefinitionLanguage/dw_ddl.sql
@@ -1,5 +1,67 @@
-CREATE TABLE [fact_compras] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
+USE master
+GO
+
+DROP DATABASE IF EXISTS seminario
+GO
+
+CREATE DATABASE seminario
+GO
+
+USE seminario
+GO
+
+DROP SCHEMA IF EXISTS project1
+GO
+
+CREATE SCHEMA project1
+GO
+
+IF OBJECT_ID('project1.fact_compras', 'U') IS NOT NULL
+    DROP TABLE project1.fact_compras;
+GO
+
+IF OBJECT_ID('project1.dim_proveedor', 'U') IS NOT NULL
+    DROP TABLE project1.dim_proveedor;
+GO
+
+IF OBJECT_ID('project1.dim_fecha', 'U') IS NOT NULL
+    DROP TABLE project1.dim_fecha;
+GO
+
+IF OBJECT_ID('project1.dim_sucursal', 'U') IS NOT NULL
+    DROP TABLE project1.dim_sucursal;
+GO
+
+IF OBJECT_ID('project1.dim_producto', 'U') IS NOT NULL
+    DROP TABLE project1.dim_producto;
+GO
+
+IF OBJECT_ID('project1.dim_marca', 'U') IS NOT NULL
+    DROP TABLE project1.dim_marca;
+GO
+
+IF OBJECT_ID('project1.dim_categoria', 'U') IS NOT NULL
+    DROP TABLE project1.dim_categoria;
+GO
+
+IF OBJECT_ID('project1.fact_ventas', 'U') IS NOT NULL
+    DROP TABLE project1.fact_ventas;
+GO
+
+IF OBJECT_ID('project1.dim_cliente', 'U') IS NOT NULL
+    DROP TABLE project1.dim_flight_status;
+GO
+
+IF OBJECT_ID('project1.dim_tipo_cliente', 'U') IS NOT NULL
+    DROP TABLE project1.dim_tipo_cliente;
+GO
+
+IF OBJECT_ID('project1.dim_vendedor', 'U') IS NOT NULL
+    DROP TABLE project1.dim_vendedor;
+GO
+
+CREATE TABLE project1.[fact_compras] (
+	[id] INTEGER NOT NULL UNIQUE,
 	[sk_proveedor] INTEGER NOT NULL,
 	[sk_categoria_prod] INTEGER NOT NULL,
 	[sk_marca_prod] INTEGER NOT NULL,
@@ -12,7 +74,7 @@ CREATE TABLE [fact_compras] (
 );
 GO
 
-CREATE TABLE [dim_proveedor] (
+CREATE TABLE project1.[dim_proveedor] (
 	[id_codigo] INTEGER NOT NULL UNIQUE,
 	[nombre] NCHAR NOT NULL,
 	[direccion] NVARCHAR NOT NULL,
@@ -22,8 +84,8 @@ CREATE TABLE [dim_proveedor] (
 );
 GO
 
-CREATE TABLE [dim_fecha] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[dim_fecha] (
+	[id] INTEGER NOT NULL UNIQUE,
 	[dia] SMALLINT NOT NULL,
 	[mes] TINYINT NOT NULL,
 	[anio] SMALLINT NOT NULL,
@@ -33,11 +95,11 @@ CREATE TABLE [dim_fecha] (
 GO
 
 CREATE INDEX [dim_fecha_index_0]
-ON [dim_fecha] ([anio]);
+ON project1.[dim_fecha] ([anio]);
 GO
 
-CREATE TABLE [dim_sucursal] (
-	[id_codigo] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[dim_sucursal] (
+	[id_codigo] INTEGER NOT NULL UNIQUE,
 	[nombre] VARCHAR NOT NULL,
 	[direccion] NVARCHAR NOT NULL,
 	[region] VARCHAR NOT NULL,
@@ -46,7 +108,7 @@ CREATE TABLE [dim_sucursal] (
 );
 GO
 
-CREATE TABLE [dim_producto] (
+CREATE TABLE project1.[dim_producto] (
 	[id] INTEGER NOT NULL,
 	[codigo] VARCHAR NOT NULL UNIQUE,
 	[nombre] NVARCHAR NOT NULL,
@@ -54,22 +116,22 @@ CREATE TABLE [dim_producto] (
 );
 GO
 
-CREATE TABLE [dim_marca] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[dim_marca] (
+	[id] INTEGER NOT NULL UNIQUE,
 	[nombre] VARCHAR NOT NULL,
 	PRIMARY KEY([id])
 );
 GO
 
-CREATE TABLE [dim_categoria] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[dim_categoria] (
+	[id] INTEGER NOT NULL UNIQUE,
 	[nombre] NVARCHAR NOT NULL,
 	PRIMARY KEY([id])
 );
 GO
 
-CREATE TABLE [fact_ventas] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[fact_ventas] (
+	[id] INTEGER NOT NULL UNIQUE,
 	[sk_categoria_prod] INTEGER NOT NULL,
 	[sk_marca_prod] INTEGER NOT NULL,
 	[sk_producto] INTEGER NOT NULL,
@@ -84,82 +146,95 @@ CREATE TABLE [fact_ventas] (
 );
 GO
 
-CREATE TABLE [dim_cliente] (
-	[id_codigo] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[dim_cliente] (
+	[id_codigo] INTEGER NOT NULL UNIQUE,
 	[nombre] NVARCHAR NOT NULL,
 	[direccion] NVARCHAR NOT NULL,
 	PRIMARY KEY([id_codigo])
 );
 GO
 
-CREATE TABLE [dim_tipo_cliente] (
-	[id] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[dim_tipo_cliente] (
+	[id] INTEGER NOT NULL UNIQUE,
 	[tipo] CHAR NOT NULL,
 	PRIMARY KEY([id])
 );
 GO
 
-CREATE TABLE [dim_vendedor] (
-	[id_codigo] INTEGER NOT NULL IDENTITY UNIQUE,
+CREATE TABLE project1.[dim_vendedor] (
+	[id_codigo] INTEGER NOT NULL UNIQUE,
 	[Nombre] NVARCHAR NOT NULL,
 	[tipo] VARCHAR NOT NULL,
 	PRIMARY KEY([id_codigo])
 );
 GO
 
-ALTER TABLE [fact_compras]
-ADD FOREIGN KEY([sk_proveedor]) REFERENCES [dim_proveedor]([id_codigo])
+ALTER TABLE project1.[fact_compras]
+ADD FOREIGN KEY([sk_proveedor]) REFERENCES project1.[dim_proveedor]([id_codigo])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_compras]
-ADD FOREIGN KEY([sk_categoria_prod]) REFERENCES [dim_categoria]([id])
+
+ALTER TABLE project1.[fact_compras]
+ADD FOREIGN KEY([sk_categoria_prod]) REFERENCES project1.[dim_categoria]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_compras]
-ADD FOREIGN KEY([sk_marca_prod]) REFERENCES [dim_marca]([id])
+
+ALTER TABLE project1.[fact_compras]
+ADD FOREIGN KEY([sk_marca_prod]) REFERENCES project1.[dim_marca]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_compras]
-ADD FOREIGN KEY([sk_producto]) REFERENCES [dim_producto]([id])
+
+ALTER TABLE project1.[fact_compras]
+ADD FOREIGN KEY([sk_producto]) REFERENCES project1.[dim_producto]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_compras]
-ADD FOREIGN KEY([sk_sucursal]) REFERENCES [dim_sucursal]([id_codigo])
+
+ALTER TABLE project1.[fact_compras]
+ADD FOREIGN KEY([sk_sucursal]) REFERENCES project1.[dim_sucursal]([id_codigo])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_compras]
-ADD FOREIGN KEY([sk_fecha]) REFERENCES [dim_fecha]([id])
+
+ALTER TABLE project1.[fact_compras]
+ADD FOREIGN KEY([sk_fecha]) REFERENCES project1.[dim_fecha]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_categoria_prod]) REFERENCES [dim_categoria]([id])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_categoria_prod]) REFERENCES project1.[dim_categoria]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_marca_prod]) REFERENCES [dim_marca]([id])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_marca_prod]) REFERENCES project1.[dim_marca]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_producto]) REFERENCES [dim_producto]([id])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_producto]) REFERENCES project1.[dim_producto]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_sucursal]) REFERENCES [dim_sucursal]([id_codigo])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_sucursal]) REFERENCES project1.[dim_sucursal]([id_codigo])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_fecha]) REFERENCES [dim_fecha]([id])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_fecha]) REFERENCES project1.[dim_fecha]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_vendedor]) REFERENCES [dim_vendedor]([id_codigo])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_vendedor]) REFERENCES project1.[dim_vendedor]([id_codigo])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_tipo_cliente]) REFERENCES [dim_tipo_cliente]([id])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_tipo_cliente]) REFERENCES project1.[dim_tipo_cliente]([id])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
-ALTER TABLE [fact_ventas]
-ADD FOREIGN KEY([sk_cliente]) REFERENCES [dim_cliente]([id_codigo])
+
+ALTER TABLE project1.[fact_ventas]
+ADD FOREIGN KEY([sk_cliente]) REFERENCES project1.[dim_cliente]([id_codigo])
 ON UPDATE NO ACTION ON DELETE NO ACTION;
 GO
